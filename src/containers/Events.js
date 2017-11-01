21259221e73bc6739f19eef847e00dd9eeb011d4
Dmytro Vasylenko <dmytro.vasylenko.ua@gmail.com>
2017-11-01 19:42:51 +0200

    test new verison of position events

--- src/containers/Events.js
+++ src/containers/Events.js
@@ -8,15 +8,16 @@ class Events extends Component {
 
 		this.state = {
 			events: [
-				{start: 0, duration: 1500, title: "Exercise"},
+				{start: 0, duration: 400, title: "Exercise"},
 				{start: 25, duration: 30, title: "Travel to work"},
 				{start: 30, duration: 30, title: "Push up branch"},
 				{start: 60, duration: 15, title: "Plan day"},
 				{start: 100, duration: 15, title: "Review yestarday's commits"},
 				{start: 180, duration: 90, title: "Code review"},
-				{start: 360, duration: 30, title: "Have lunch with John"},
-				// {start: 370, duration: 45, title: "Skype call"},
-				{start: 380, duration: 20, title: "Follow up with designer"}
+				{start: 360, duration: 15, title: "Have lunch with John"},
+				{start: 370, duration: 45, title: "Skype call"},
+				{start: 380, duration: 20, title: "Follow up with designer"},
+				{start: 390, duration: 10, title: "Follow up with designer"}
 			]
 		};
 	}
@@ -54,27 +55,34 @@ class Events extends Component {
 				continue;
 			}
 			let columns = [];
-			for(let item of cluster) {
-				let inserted = false;
-				for(let i = 0; i < columns.length; i++) {
-					if(inserted) {
+			for(let event of cluster) {
+				let canInserted = false;
+				let columnPoint;
+				for(let column of columns) {
+					if(this.canPut(column, event)) {
+						canInserted = true;
+						columnPoint = column;
 						break;
 					}
-					for(let j = 0; j < columns[i].length; j++) {
-						if(!this.eventsCrossed(item, columns[i][j])) {
-							columns[i].push(item);
-							inserted = true;
-							break;
-						}
-					}
 				}
-				if(!inserted) {
-					columns.push([item]);
+				if(!canInserted) {
+					columns.push([event]);
+				} else {
+					columnPoint.push(event);
 				}
 			}
 			result.push(columns);
 		}
 		return result;
+	}
+
+	canPut(events, newEvent) {
+		for(let event of events) {
+			if(this.eventsCrossed(event, newEvent)) {
+				return false;
+			}
+		}
+		return true;
 	}
 
 	eventsCrossed(eventA, eventB) {
